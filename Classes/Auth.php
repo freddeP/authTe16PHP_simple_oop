4b57547fb2a829c83193c913375b0c0aeb5d39d8
Fredric Persson <freper7912@skynet.halmstad.se>
2018-11-16 15:21:10 +0100

    sign_in påbörjad

--- Classes/Auth.php
+++ Classes/Auth.php
@@ -17,13 +17,49 @@ class Auth {
 
     public static function sign_up($user)
     {
+        // Har skall $user först valideras osv
+
        self::add_user_to_file($user);
        return "User was added to DB";
 
     }
-
-    public function sign_in($email, $password)
+    public static function sign_in($email_input, $password)
     {
+        // hämta alla användare
+        $users = self::get_all_users(); 
+        $emailExists = false;
+        $index = "";
+        foreach($users as $key => $user)
+        {
+            if($user->email == $email_input)
+            {
+                $emailExists = true;
+                $index = $key;
+                break;
+            }
+        }
+        
+        
+        echo $emailExists. "<hr>";
+       // Om email existerar 
+       if($emailExists)
+       {
+            $hash = $users[$index]->password;
+            $pwCheck = password_verify($password,$hash);
+            if($pwCheck)
+            {
+                return true;
+            }
+            else{
+                return "wrong password";
+            }
+
+       } 
+       else
+       {
+           return "no such user";
+       }
+        
 
     }
 
--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require("./Classes/Auth.php");
 require("./Classes/User.php");
 
 // Skapa ny user
-$user = new User("test@home.se", "Hej");
+//$user = new User("test@kalleanka.se", "Hej");
 // Skicka nu denna user till sign_up
-Auth::sign_up($user);
+//Auth::sign_up($user);
+
+echo Auth::sign_in("test@kalleanka.se", "Hj");
